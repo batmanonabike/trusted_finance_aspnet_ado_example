721062553602952a94de90a4d91d8416ff435c67
Martyn C Brown <batmanonabike@outlook.com>
2026-08-23 19:14:48 +0100

    Some useful logging at startup for the reviewer of the IRate web api.

--- TrustedFinanceExchangeRate/TrustedExchangeWebApi/Program.cs
+++ TrustedFinanceExchangeRate/TrustedExchangeWebApi/Program.cs
@@ -21,7 +21,22 @@ namespace TrustedExchangeWebApi
 
             app.UseAuthorization();
             app.MapControllers();
+
+            app.Lifetime.ApplicationStarted.Register(() => LogStartupUrls(app));
+
             app.Run();
+        }
+
+        private static void LogStartupUrls(WebApplication app)
+        {
+            var baseUrl = app.Urls.FirstOrDefault() ?? "http://localhost:5165";
+            app.Logger.LogInformation(
+                "Try it:\n" +
+                "  1) {CodesUrl} <- returns all supported currency codes\n" +
+                "  2) {EurUrl} <- returns the exchange rate for EUR\n" +
+                "  3) {GbpUrl} <- returns the exchange rate for GBP\n" +
+                "(rates are cached for a while, so repeat hits will be faster)",
+                $"{baseUrl}/rates/currencycodes", $"{baseUrl}/rates/EUR", $"{baseUrl}/rates/GBP");
         }
     }
 }
